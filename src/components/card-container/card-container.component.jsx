d6b71e005330172eb96eb9728ccf81232f5215b1
Sameer Jain <72691195+Sameer4522@users.noreply.github.com>
2022-11-30 14:32:07 +0530

    first commit

---
+++ src/components/card-container/card-container.component.jsx
@@ -0,0 +1,18 @@
+import "./card-container.styles.css";
+
+const CardContainer = ({ monster }) => {
+  const { name, id, email } = monster;
+
+  return (
+    <div className="card-container" key={id}>
+      <img
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+        alt={`monster ${name}`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
+
+export default CardContainer;
---
+++ src/components/card-list/card-list.components.jsx
@@ -0,0 +1,14 @@
+import "./card-list.styles.css";
+import CardContainer from "../card-container/card-container.component";
+
+const CardList = ({ monsters }) => {
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => {
+        return <CardContainer monster={monster} />;
+      })}
+    </div>
+  );
+};
+
+export default CardList;
